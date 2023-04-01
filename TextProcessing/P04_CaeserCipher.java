2913452f8e5b6380c03d674bb4c55752c79c21d1
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-04-01 22:43:27 +0300

    Text - Processing

---
+++ TextProcessing/P04_CaeserCipher.java
@@ -0,0 +1,17 @@
+package TextProcessing;
+
+import java.util.Scanner;
+
+public class P04_CaeserCipher {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (char symbol : text.toCharArray()) {
+            char encryptedSymbol = (char)(symbol + 3);
+            encryptedText.append(encryptedSymbol);
+        }
+        System.out.println(encryptedText);
+    }
+}
