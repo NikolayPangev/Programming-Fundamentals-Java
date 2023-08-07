46b9cf4e923526e1c7f46388a0b721a36a76ff60
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-08-07 17:36:24 +0300

    Basic Syntax Conditional Statements And Loops - Lab

--- BasicSyntaxConditionalStatementsAndLoops/BasicSyntaxConditionalStatementsAndLoops_Lab/_10_MultiplicationTable.java
+++ BasicSyntaxConditionalStatementsAndLoops/BasicSyntaxConditionalStatementsAndLoops_Lab/_10_MultiplicationTable.java
@@ -1,17 +1,13 @@
-package BasicSyntax_ConditionalStatementsAndLoops_Lab;
+package BasicSyntaxConditionalStatementsAndLoops_Lab;
 
 import java.util.Scanner;
 
-public class P10_MultiplicationTable {
+public class _10_MultiplicationTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int number = Integer.parseInt(scanner.nextLine());
-        int product = 0;
-
         for (int i = 1; i <= 10; i++) {
-            product = i * number;
-            System.out.printf("%d X %d = %d%n", number, i, product);
+            System.out.printf("%d X %d = %d%n", number, i, number * i);
         }
     }
 }
