7096b5806dfd2daa8c85a07f4cfdedeb2161b4da
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-08-07 17:32:18 +0300

    Basic Syntax Conditional Statements And Loops - Lab

--- BasicSyntaxConditionalStatementsAndLoops/BasicSyntaxConditionalStatementsAndLoops_Lab/_09_SumOfOddNumbers.java
+++ BasicSyntaxConditionalStatementsAndLoops/BasicSyntaxConditionalStatementsAndLoops_Lab/_09_SumOfOddNumbers.java
@@ -1,14 +1,13 @@
-package BasicSyntax_ConditionalStatementsAndLoops_Lab;
+package BasicSyntaxConditionalStatementsAndLoops_Lab;
 
 import java.util.Scanner;
 
-public class P09_SumOfOddNumbers {
+public class _09_SumOfOddNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int number = Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
         int sum = 0;
-        for (int i = 0; i < number * 2; i++) {
+        for (int i = 0; i < n * 2; i++) {
             if (i % 2 == 1){
                 sum+=i;
                 System.out.println(i);
