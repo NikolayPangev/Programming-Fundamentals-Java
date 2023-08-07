95cf67e2e2f50ae129800c071ee3962779319192
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-08-07 16:45:44 +0300

    Basic Syntax Conditional Statements And Loops - Lab

--- BasicSyntaxConditionalStatementsAndLoops/BasicSyntaxConditionalStatementsAndLoops_Lab/_01_StudentInformation.java
+++ BasicSyntaxConditionalStatementsAndLoops/BasicSyntaxConditionalStatementsAndLoops_Lab/_01_StudentInformation.java
@@ -1,11 +1,10 @@
-package BasicSyntax_ConditionalStatementsAndLoops_Lab;
+package BasicSyntaxConditionalStatementsAndLoops_Lab;
 
 import java.util.Scanner;
 
-public class P01_Student_Information {
+public class _01_StudentInformation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String name = scanner.nextLine();
         int age = Integer.parseInt(scanner.nextLine());
         double grade = Double.parseDouble(scanner.nextLine());
