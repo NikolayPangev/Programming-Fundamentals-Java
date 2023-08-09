0b0e43b0c85024bfc13951c8423bb7f462607cce
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-08-09 22:49:21 +0300

    Basic Syntax Conditional Statements And Loops - Exercise

--- BasicSyntaxConditionalStatementsAndLoops/BasicSyntaxConditionalStatementsAndLoops_Exercise/_01_Ages.java
+++ BasicSyntaxConditionalStatementsAndLoops/BasicSyntaxConditionalStatementsAndLoops_Exercise/_01_Ages.java
@@ -1,16 +1,15 @@
-package BasicSyntax_ConditionalStatementsAndLoops_Exercise;
+package BasicSyntaxConditionalStatementsAndLoops_Exercise;
 
 import java.util.Scanner;
 
-public class P01_Ages {
+public class _01_Ages {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = Integer.parseInt(sc.nextLine());
 
-        Scanner scanner = new Scanner(System.in);
-        int age = Integer.parseInt(scanner.nextLine());
-
-        if(age >= 0 && age <= 2){
+        if (age >= 0 && age <= 2) {
             System.out.println("baby");
-        } else if (age >= 3 && age <= 13){
+        } else if (age >= 3 && age <= 13) {
             System.out.println("child");
         } else if (age >= 14 && age <= 19) {
             System.out.println("teenager");
@@ -21,4 +20,3 @@ public class P01_Ages {
         }
     }
 }
-
